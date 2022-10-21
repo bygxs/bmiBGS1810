8383baed82840fa08f54867a5e03d47fc1a5f9b4
bygxs <114972065+bygxs@users.noreply.github.com>
2022-10-21 11:00:02 +0200

    the app keeps stopping. something that got to do with onClick

---
+++ app/src/main/java/com/biniyam/bmibgs1810/MainActivity.java
@@ -0,0 +1,62 @@
+package com.biniyam.bmibgs1810;
+
+import static com.biniyam.bmibgs1810.R.id.btnBmi;
+import static com.biniyam.bmibgs1810.R.id.textMessage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    EditText      height;
+    EditText     weight;
+    Button btnCalc;
+    TextView resultText;
+
+    //TextView  setCalculation();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView txtMessage = findViewById(R.id.textMessage); //Ask for height message
+      //  txtMessage.setText("Hello");       //change this to Know your BMI now. @string?
+
+         height = findViewById(R.id.inputHeight);
+         weight = findViewById(R.id.inputWeight);
+         resultText = findViewById(R.id.showResult);
+
+
+        Button btnCalc = findViewById(btnBmi);
+
+        btnCalc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setCalculation();
+            }
+        });
+    }
+
+ public void setCalculation (){
+        int h = Integer.parseInt(height.getText().toString()) ;  // int datatype cuts of decimal point
+        int w = Integer.parseInt(weight.getText().toString());
+        int heightValue = (h)*(h);
+        int weightValue = (w) ;
+
+      int bmi =weightValue / (heightValue*heightValue);
+     resultText.setText((int) bmi);
+
+ }
+
+}
+// TODO Complete this: getInputs from user and save them in a variable calculate to BMI
+//  and then setResultBMI in a TextView them to a result BMI
